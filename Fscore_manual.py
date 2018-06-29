d83ec84362d7f10fe83cdc240f87f30e6365d27f
Mahtab Ahmed <navid5792@yahoo.com>
2018-06-29 13:01:18 -0400

    Fix metrics

--- Fscore_manual.py
+++ Fscore_manual.py
@@ -3,47 +3,43 @@
 """
 Created on Mon Jun 25 20:25:34 2018
 
-@author: bob
+@author: Jumayel
 """
 from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_fscore_support
 import pickle
-true, pred = pickle.load(open('./true pred/true_pred_HPRD50.pkl', 'rb'))
+true, pred, _ = pickle.load(open('./true pred/true_pred_IEPA.pkl', 'rb'))
 prec, reca, fscore, sup = precision_recall_fscore_support(true, pred, average='macro')
 print("precision: ", prec, " recall: ", reca, " fscore: ", fscore)
 
-def performance_metrics(label, labels, actual_labels, predicted_labels):
-    true_positive = 0
-    false_positive = 0
-    false_negative = 0
+def perf_measure(y_actual, y_hat):
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
 
-    other = list(set(labels) - set([label]))
-    for i in range(len(predicted_labels)):
-        actual = actual_labels[i]
-        predicted = predicted_labels[i]
-        if actual == label and predicted == label:
-            true_positive += 1
-        if actual in other and predicted == label:
-            false_positive += 1
-        if actual == label and predicted in other:
-            false_negative += 1
+    for i in range(len(y_hat)): 
+        if y_actual[i]==y_hat[i]==1:
+           TP += 1
+        if y_hat[i]==1 and y_actual[i]!=y_hat[i]:
+           FP += 1
+        if y_actual[i]==y_hat[i]==0:
+           TN += 1
+        if y_hat[i]==0 and y_actual[i]!=y_hat[i]:
+           FN += 1
 
-    precision = true_positive/(true_positive + false_positive)
-    recall = true_positive/(true_positive + false_negative)
-    fscore = 2*precision*recall/(precision + recall)
+    return(TP, FP, TN, FN)
+   
 
-    return precision, recall, fscore  
+tp, fp, tn, fn = perf_measure(true, pred)
+prec_pos = tp/(tp+fp)
+prec_neg = tn/(tn+fn)
 
-precision = []
-recall = []
-fscore = []   
-    
-for label in [0,1]:
-   pre , re, fs = performance_metrics(label,[0,1],true,pred)
-   precision.append(pre)
-   recall.append(re)
-   fscore.append(fs)
-print(precision, recall, fscore)
-import numpy as np
-print(np.mean(precision))
-print(np.mean(recall))
-print(np.mean(fscore))
+rec_pos = tp/(tp+fn)
+rec_neg = tn/(tn+fp)
+
+fscore_pos = 2*prec_pos*rec_pos/(prec_pos + rec_pos)
+fscore_neg = 2*prec_neg*rec_neg/(prec_neg + rec_neg)
+
+print('Precision (Avg.): ', (prec_pos + prec_neg)/2)
+print('Recall (Avg.): ', (rec_pos + rec_neg)/2)
+print('Fscore (Avg.): ', (fscore_pos + fscore_neg)/2)
